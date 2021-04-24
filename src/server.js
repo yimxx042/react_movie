df54851f9a439492517c2d307ecb1578a3c0ef7d
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-04-23 19:42:52 -0500

    server issue

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const jsonServer = require('../json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 server.use(middlewares)
 server.use(router);
 server.listen(PORT, () => {
