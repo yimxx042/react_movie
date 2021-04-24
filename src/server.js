aa45ec7e9e109b5ece70134180a03b7f3447646d
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-04-23 19:37:57 -0500

    fix server error

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('../json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('../json-server/db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8000;
 server.use(middlewares)
