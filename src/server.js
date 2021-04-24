085d4d4b07f7ba68bbc4f01404f21c24300a7d82
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-04-23 19:34:19 -0500

    changed server file

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
-const jsonServer = require('json-server');
+const jsonServer = require('../json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('../json-server/db.json');
-const middlewares = jsonServer.defaults({
-    static: './build'
-});
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8000;
-ServiceUIFrameContext.use(middlewares);
-server.use(jsonServer.rewriter({
-    '/*': '/$1',
-}))
+server.use(middlewares)
 server.use(router);
 server.listen(PORT, () => {
     console.log("Server is running");
